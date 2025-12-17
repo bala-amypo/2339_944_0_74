c513f896452d85e3745b6c37474e9b2246267bc4
bala-amypo <bala@amypo.in>
2025-12-17 08:03:40 +0000

    User commit

--- src/main/java/com/example/demo/NewEntity/StudentValidation.java
+++ src/main/java/com/example/demo/NewEntity/StudentValidation.java
@@ -1,6 +1,13 @@
-package com.example.demp.newentity;
+package com.example.demo.newentity;
 import jarkarata.presistence.Id;
+import jarkarata.Validation.constraints.NotBlank;
 public class Validation{
-    @Idprivate long Id
-    @NotBlank(message=)
+    @Id
+    @GeneratedValue(stargery)
+    private long Id
+    @NotBlank(message="No permission for outing")
+    private string name;
+    @NotBlank(message="Should not contain space")
+    @Email(message="Invalid fromat")
+    private string email;
 }
